64fd710195053dfe863756c97bbd7a44a31b19df
Amotul-raheem <amotulraheem1499@gmail.com>
2024-04-28 22:16:07 +0100

    destructure product details at the top of the product component

--- client/pages/product.jsx
+++ client/pages/product.jsx
@@ -4,7 +4,22 @@ import {products} from "../../server/db";
 export default function Product() {
     const [basketItems, setBasketItems] = useState(0)
     const [currentQuantity, setCurrentQuantity] = useState(1)
-    const product = products[0]
+    const {
+        name,
+        power,
+        price,
+        quantity,
+        colour,
+        model_code,
+        length,
+        width,
+        height,
+        weight,
+        brand,
+        description,
+        img_url,
+        id
+    } = products[0]
 
     const handleAddToCart = () => {
         setBasketItems(basketItems + currentQuantity)
@@ -50,7 +65,7 @@ export default function Product() {
                     <div className="flex w-full justify-center sm:w-full md:w-full lg:w-[45%]">
                         <img
                             className=" rounded-3xl w-full h-full"
-                            src={product.img_url}
+                            src={img_url}
                             alt="Basket"
                         />
                     </div>
@@ -58,12 +73,12 @@ export default function Product() {
                         <h1 className="font-bold font-sans text-ice my-10
                         text-4xl sm:text-4xl md:text-6xl lg:text-7xl
                         leading-[3rem] sm:leading-3 md:leading-[5rem] lg:leading-[6rem] "
-                        >{product.name}</h1>
-                        <p className="my-4 font-medium text-2xl font-sans text-purpleHaze">{product.power} // Packet
-                            of {product.quantity}</p>
+                        >{name}</h1>
+                        <p className="my-4 font-medium text-2xl font-sans text-purpleHaze">{power} // Packet
+                            of {quantity}</p>
                         <div
                             className="w-full flex justify-between items-center  my-6 font-sans  sm:my-6 md:my-16 lg:my-16">
-                            <p className="font-bold font-sans text-4xl sm:text-4xl md:text-5xl lg:text-5xl mt-12 ">&pound;{product.price / 100}</p>
+                            <p className="font-bold font-sans text-4xl sm:text-4xl md:text-5xl lg:text-5xl mt-12 ">&pound;{price / 100}</p>
                             <div className="flex flex-col justify-center items-center">
                                 <p className="font-sans text-lg py-2 ">Qty</p>
                                 <div className="flex justify-center items-center">
@@ -72,7 +87,9 @@ export default function Product() {
                                         className={`flex mx-4 ${currentQuantity === 1 ? 'bg-plum' : 'bg-sohoLights'} rounded-2xl justify-center items-center cursor-pointer
                                         w-[50px] sm:w-[50px] md:w-[60px] lg:w-[60px]
                                         h-[50px] sm:h-[50px] md:h-[60px]  lg:h-[60px]`}>
-                                        <button disabled={currentQuantity === 1} className={`font-sans font-bold ${currentQuantity === 1 ? 'text-purpleHaze' : 'text-siphon'} text-2xl`}> - </button>
+                                        <button disabled={currentQuantity === 1}
+                                                className={`font-sans font-bold ${currentQuantity === 1 ? 'text-purpleHaze' : 'text-siphon'} text-2xl`}> -
+                                        </button>
                                     </div>
                                     <p title="Current quantity"
                                        className={`font-bold font-sans  text-3xl sm:text-3xl md:text-5xl lg:text-5xl`}>{currentQuantity}</p>
@@ -81,7 +98,7 @@ export default function Product() {
                                         className="flex mx-4 bg-sohoLights rounded-2xl justify-center items-center cursor-pointer
                                         w-[50px] sm:w-[50px] md:w-[60px] lg:w-[60px]
                                         h-[50px] sm:h-[50px] md:h-[60px] lg:h-[60px]">
-                                        <button className="font-sans font-bold text-siphon text-2xl"> + </button>
+                                        <button className="font-sans font-bold text-siphon text-2xl"> +</button>
                                     </div>
                                 </div>
                             </div>
@@ -99,7 +116,7 @@ export default function Product() {
             </div>
             <div className="bg-hemocyanin my-4 px-6 pt-8 pb-16 w-full h-auto sm:px-6 md:px-10 lg:px-16 ">
                 <h1 className="text-3xl font-medium font-sans my-8 sm:text-3xl md:text-5xl lg:text-5xl">Description</h1>
-                <p className="w-full sm:w-full md:w-full lg:w-[50%] text-lg sm:text-lg md:text-2xl lg:text-2xl font-sans">{product.description}</p>
+                <p className="w-full sm:w-full md:w-full lg:w-[50%] text-lg sm:text-lg md:text-2xl lg:text-2xl font-sans">{description}</p>
             </div>
             <div className=" my-4 px-6 py-8 w-full h-auto sm:px-6 md:px-10 lg:px-16 ">
                 <h1 className="text-3xl font-medium font-sans my-8 sm:text-3xl md:text-5xl lg:text-5xl">Specification</h1>
@@ -112,11 +129,11 @@ export default function Product() {
                         <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">Cool Daylight</p>
                     </div>
                     <div>
-                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{product.brand}</p>
-                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{product.weight}</p>
-                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{product.height} x {product.width} x {product.length} </p>
-                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{product.model_code}</p>
-                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{product.colour}</p>
+                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{brand}</p>
+                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{weight}</p>
+                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{height} x {width} x {length} </p>
+                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{model_code}</p>
+                        <p className="my-6 font-sans text-lg sm:text-lg md:text-2xl lg:text-2xl">{colour}</p>
                     </div>
 
                 </div>
